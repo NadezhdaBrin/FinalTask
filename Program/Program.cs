d1c882e6eea8de87c347a276f9bafc803f23463f
NadiaBrin <nadiabrin@gmail.com>
2022-10-05 12:54:51 +0300

    программа

---
+++ Program/Program.cs
@@ -0,0 +1,44 @@
+﻿// Программа, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+void createArr(string[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элеммент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+}
+
+void createNewArr(string[] arr) 
+{
+    string[] newarr = new string[arr.Length]; 
+    int count = 0;                                        
+    for (int i = 0; i < arr.Length; i++)      
+    {
+        if (arr[i].Length <= 3)
+        {
+            newarr[count] = arr[i];
+            count++;
+        }
+    }
+    Console.WriteLine("Новый массив");        
+    Console.WriteLine(string.Join(" ", newarr));
+}
+
+void PrintArray(string[] arr)       
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Задайте количество строк: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[length];
+createArr(array);
+PrintArray(array);
+Console.WriteLine();
+createNewArr(array);
